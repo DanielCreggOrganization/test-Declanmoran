d2e8ebd5b78588e4034c8e4c32ee8a7e0a380f2e
Declanmoran <g00372047@gmit.ie>
2020-01-27 17:47:12 +0000

    Week 3

---
+++ src/ie/gmit/lecw3/Car.java
@@ -0,0 +1,43 @@
+package ie.gmit.lecw3;
+
+/**
+ * Car
+ */
+public class Car {
+    //Instance Variables
+    int topSpeed;
+    int totalSeats;
+    int fuelCapacity;
+    String manufacturer;
+    String colour;
+    String owner;
+    float engineSize;
+    float value;
+    
+
+    //Class Methods
+
+    void show(){
+        System.out.println(manufacturer);
+    }
+
+    void refuel(){
+    
+    }
+
+    void park(){
+
+    }
+
+    void drive(){
+
+    }
+
+    void crash(){
+         value = 0;
+    }
+
+
+
+    
+}
---
+++ src/ie/gmit/lecw3/DataType.java
@@ -0,0 +1,20 @@
+package ie.gmit.lecw3;
+
+
+public class DataType {
+
+    public static void main (String[] args) {
+        boolean isAlive = true;
+        byte age = 100;
+        byte week = 3;
+        short year = 2020;
+        int irepop = 4830000;
+        long worldpop = 7600000000L;
+
+        System.out.println(age);
+        System.out.println(week);
+        System.out.println(year);
+        System.out.println(irepop);
+        System.out.println(worldpop);
+    }
+}
